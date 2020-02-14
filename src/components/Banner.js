9991048249f589039c26e2b5880ba3e5732fdeff
Jay\^.^/ <stsend90@yahoo.com>
2020-02-13 19:03:05 -0500

    adding image functions

---
+++ src/components/Banner.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const style = {
+  logoIcon: {
+    width: 250,
+    heigth: 200
+  },
+  bgDark: {
+    backgroundColor: `rgb(1,0,1)`
+  }
+}
+
+const Banner = props => (
+  <article style={style.bgDark} className="mw7 center ph3 ph5-ns tc br2 pv3 bg-black-90 mb5 animated fadeIn">
+    <img style={style.logoIcon} className='logoIcon' src="./img/tenor.gif" alt="naruto"/>
+    <h2 className="fw2 f4 lh-copy mt0 mb3 white animated fadeIn slower">
+    Click on an image to earn points, but don't click on any more than once!
+    </h2>
+  </article>
+);
+
+export default Banner;
