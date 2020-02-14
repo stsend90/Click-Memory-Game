81bc934f3cda83165a04a345406dac9a01849bdf
Jay\^.^/ <stsend90@yahoo.com>
2020-02-13 21:28:34 -0500

    added border on img

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -13,9 +13,9 @@ const style = {
 const Banner = props => (
   <article style={style.bgDark} className="mw7 center ph3 ph5-ns tc br2 pv3 bg-black-90 mb5 animated fadeIn">
     <img style={style.logoIcon} className='logoIcon' src="./img/tenor.gif" alt="naruto"/>
-    <h2 className="fw2 f4 lh-copy mt0 mb3 white animated fadeIn slower">
+    <h4 className="fw2 f4 lh-copy mt0 mb3 white animated fadeIn slower">
     Click on an image to earn points, but don't click on any more than once!
-    </h2>
+    </h4>
   </article>
 );
 
