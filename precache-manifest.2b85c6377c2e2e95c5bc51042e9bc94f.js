a20ef098a0baaae0a907e72aff3f2470910ebe95
Jay\^.^/ <stsend90@yahoo.com>
2020-02-25 16:32:55 -0500

    Updates

---
+++ precache-manifest.2b85c6377c2e2e95c5bc51042e9bc94f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fedf5d2d633bc5f9b37523630ee5d734",
+    "url": "/Click-Memory-Game/index.html"
+  },
+  {
+    "revision": "e5ed7bd71e5f30152364",
+    "url": "/Click-Memory-Game/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "1f4fe018b8f60225364e",
+    "url": "/Click-Memory-Game/static/css/main.51ac19f4.chunk.css"
+  },
+  {
+    "revision": "e5ed7bd71e5f30152364",
+    "url": "/Click-Memory-Game/static/js/2.c7977605.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Click-Memory-Game/static/js/2.c7977605.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f4fe018b8f60225364e",
+    "url": "/Click-Memory-Game/static/js/main.eb923fc3.chunk.js"
+  },
+  {
+    "revision": "67373bd950b020253713",
+    "url": "/Click-Memory-Game/static/js/runtime-main.f9830d96.js"
+  }
+]);
